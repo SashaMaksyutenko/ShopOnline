702d293833cf37335a0befda98aa16b0bf99a809
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2022-09-04 17:10:57 +0300

    commit 16

--- ShopOnline.Web/Program.cs
+++ ShopOnline.Web/Program.cs
@@ -9,8 +9,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:4284/") });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IShoppingCartService, ShoppingCartService>();
 builder.Services.AddBlazoredLocalStorage();
